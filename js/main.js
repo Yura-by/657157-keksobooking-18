35fa32ccb9d426fc905fc84070f608a2cf257307
Yura-by <s.older681@gmail.com>
2019-09-22 00:39:44 +0300

    Поправка по замечаниям наставника

--- js/main.js
+++ js/main.js
@@ -1,24 +1,6 @@
 'use strict';
 
-var ADVERTS_QUANTITY = 8;
-var HOUSING_TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var HOUSING_CHECK_TIMES = ['12:00', '13:00', '14:00'];
-var HOUSING_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var HOUSING_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var HOUSING_MAX_ADRESS = 1100;
-var HOUSING_MIN_ADRESS = 100;
-var MAX_PRICE = 50000;
-var MIN_PRICE = 1000;
-var MAX_ROOMS = 3;
-var MIN_ROOMS = 1;
-var MAX_QUESTS = 12;
-var MIN_QUESTS = 1;
-var MAX_PIN_X = 1150;
-var MIN_PIN_X = 50;
-var MAX_PIN_Y = 630;
-var MIN_PIN_Y = 130;
-
-var advertsData = {
+var mokiData = {
   QUANTITY: 8,
   TYPES: ['palace', 'flat', 'house', 'bungalo'],
   TIMES: ['12:00', '13:00', '14:00'],
@@ -39,7 +21,7 @@ var advertsData = {
   pinCoordinates: {
     X: {
       MAX: 1150,
-      MIN: 50
+      MIN: 0
     },
     Y: {
       MAX: 630,
@@ -63,39 +45,37 @@ var getRandomNumber = function (maxValue, minValue) {
   return result;
 };
 
-var getMokiData = function (housingTypes, housingChecktimes, housingFeatures, housingPhotos) {
+var getRandomLengthArray = function (array) {
+  var resultArray = array.slice();
+  resultArray.length = getRandomNumber(array.length);
+  return resultArray;
+};
+
+var getMokiData = function (advertsData) {
   var adverts = [];
-  for (var i = 0; i < ADVERTS_QUANTITY; i++) {
-    var featuresList = [];
-    for (var counter = 0; counter < getRandomNumber(housingFeatures.length); counter++) {
-      featuresList.push(housingFeatures[counter]);
-    }
-    var photosList = [];
-    for (var counterPhoto = 0; counterPhoto < getRandomNumber(housingPhotos.length); counterPhoto++) {
-      photosList.push(housingPhotos[counterPhoto]);
-    }
+  for (var i = 0; i < advertsData.QUANTITY; i++) {
     var advert = {
       author: {
         avatar: 'img/avatars/user' + '0' + (i + 1) + '.png'
       },
 
       offer: {
-        title: 'Отличный выбор',
-        adress: getRandomNumber(HOUSING_MAX_ADRESS, HOUSING_MIN_ADRESS) + ', ' + getRandomNumber(HOUSING_MAX_ADRESS, HOUSING_MIN_ADRESS),
-        price: getRandomNumber(MAX_PRICE, MIN_PRICE),
-        type: housingTypes[getRandomNumber(housingTypes.length - 1)],
-        rooms: getRandomNumber(MAX_ROOMS, MIN_ROOMS),
-        quests: getRandomNumber(MAX_QUESTS, MIN_QUESTS),
-        checkin: housingChecktimes[getRandomNumber(housingChecktimes.length - 1)],
-        checkout: housingChecktimes[getRandomNumber(housingChecktimes.length - 1)],
-        features: featuresList,
-        description: '',
-        photos: photosList
+        title: advertsData.TYPES[getRandomNumber(advertsData.TYPES.length - 1)],
+        adress: getRandomNumber(advertsData.pinCoordinates.X.MAX, advertsData.pinCoordinates.X.MIN) + ', ' + getRandomNumber(advertsData.pinCoordinates.Y.MAX, advertsData.pinCoordinates.Y.MIN),
+        price: getRandomNumber(advertsData.price.MAX, advertsData.price.MIN),
+        type: advertsData.TYPES[getRandomNumber(advertsData.TYPES.length - 1)],
+        rooms: getRandomNumber(advertsData.rooms.MAX, advertsData.rooms.MIN),
+        quests: getRandomNumber(advertsData.quests.MAX, advertsData.quests.MIN),
+        checkin: advertsData.TIMES[getRandomNumber(advertsData.TIMES.length - 1)],
+        checkout: advertsData.TIMES[getRandomNumber(advertsData.TIMES.length - 1)],
+        features: getRandomLengthArray(advertsData.FEATURES),
+        description: advertsData.TYPES[getRandomNumber(advertsData.TYPES.length - 1)],
+        photos: getRandomLengthArray(advertsData.PHOTOS)
       },
 
       location: {
-        x: getRandomNumber(MAX_PIN_X, MIN_PIN_X),
-        y: getRandomNumber(MAX_PIN_Y, MIN_PIN_Y)
+        x: getRandomNumber(advertsData.pinCoordinates.X.MAX, advertsData.pinCoordinates.X.MIN),
+        y: getRandomNumber(advertsData.pinCoordinates.Y.MAX, advertsData.pinCoordinates.Y.MIN)
       }
     };
     adverts.push(advert);
@@ -114,13 +94,13 @@ var renderPin = function (pinData) {
   return pin;
 };
 
-var addElement = function (housingTypes, housingChecktime, housingFeatures, housingPhotos) {
-  var pins = getMokiData(housingTypes, housingChecktime, housingFeatures, housingPhotos);
+var addPins = function (pinsInside) {
+  var pins = getMokiData(pinsInside);
   for (var j = 0; j < pins.length; j++) {
     fragment.appendChild(renderPin(pins[j]));
   }
 };
 
-addElement(HOUSING_TYPES, HOUSING_CHECK_TIMES, HOUSING_FEATURES, HOUSING_PHOTOS);
+addPins(mokiData);
 
 pinList.appendChild(fragment);
